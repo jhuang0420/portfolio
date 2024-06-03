9ff0839d4fca781b372e8e28118ab9192f5f0bc0
Jeffrey Huang <jhuang0420@gmail.com>
2024-06-03 14:42:43 -0400

    update

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ function About() {
         <section id="about" className="about">
             <div className="about-text">
                 <h2>About Me</h2>
-                <p> poop Hi, I'm Jeffrey Huang. I reside in Queens, NY, and I'm an aspiring software developer eager to apply my skills in a professional setting. My technical experience is centered around full-stack web development, familiarizing myself with tools such as React, Flutter, Flask, etc. I am passionate about creating effective and efficient solutions while working in a team setting to gain invaluable interpersonal and technical skills. </p>
+                <p> Hi, I'm Jeffrey Huang. I reside in Queens, NY, and I'm an aspiring software developer eager to apply my skills in a professional setting. My technical experience is centered around full-stack web development, familiarizing myself with tools such as React, Flutter, Flask, etc. I am passionate about creating effective and efficient solutions while working in a team setting to gain invaluable interpersonal and technical skills. </p>
             </div>
             <div className="about-image">
                 <img src={logo} alt="Logo" className="logo" />
