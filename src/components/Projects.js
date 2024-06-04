3e4af93777b6767dbc4a102e129244b55eef636b
Jeffrey Huang <jhuang0420@gmail.com>
2024-06-04 15:28:00 -0400

    updated projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,37 +2,47 @@
 import React from 'react';
 import '../style/Projects.css';
 import project1Image from '../assests/p1.jpg';
-import project2Image from '../assests/p1.jpg';
-import project3Image from '../assests/p1.jpg';
 
 function Projects() {
     return (
         <section id="projects" className="projects">
-            
-            <div className="project-card">
-                <a href="#project1">
-                    <img src={project1Image} alt="Project 1" className="project-image" />
-                </a>
-                <div className="project-title">Project 1</div>
-                <div className="project-description">Description of Project 1</div>
 
-            </div>
             <div className="project-card">
-                <a href="#project2">
-                    <img src={project2Image} alt="Project 2" className="project-image" />
-                </a>
-                <div className="project-title">Project 2</div>
-                <div className="project-description">Description of Project 2</div>
-
+                <div className="img">
+                    <a href="https://github.com/jhuang0420/portfolio/tree/master" target="_blank" rel="noopener noreferrer">
+                        <img src={project1Image} alt="Project 1" className="project-image" />
+                    </a>
+                </div>
+                <div className="txt">
+                    <div className="project-title">Project 1</div>
+                    <div className="project-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vel lectus sit amet metus egestas facilisis. Mauris mattis ante in metus facilisis, ut porttitor neque pretium. Praesent hendrerit massa lacus, non elementum nisl vulputate ac. Praesent nibh velit, posuere mattis leo eu, vestibulum interdum metus. Morbi pellentesque ultricies blandit. Nullam rutrum consequat iaculis.</div>
+                </div>
             </div>
+
             <div className="project-card">
-                <a href="#project3">
-                    <img src={project3Image} alt="Project 3" className="project-image" />
-                </a>
-                <div className="project-title">Project 3</div>
-                <div className="project-description">Description of Project 3</div>
-
+                <div className="txt">
+                    <div className="project-title">Project 1</div>
+                    <div className="project-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vel lectus sit amet metus egestas facilisis. Mauris mattis ante in metus facilisis, ut porttitor neque pretium. Praesent hendrerit massa lacus, non elementum nisl vulputate ac. Praesent nibh velit, posuere mattis leo eu, vestibulum interdum metus. Morbi pellentesque ultricies blandit. Nullam rutrum consequat iaculis.</div>
+                </div>
+                <div className="img">
+                    <a href="https://github.com/jhuang0420/portfolio/tree/master" target="_blank" rel="noopener noreferrer">
+                        <img src={project1Image} alt="Project 1" className="project-image" />
+                    </a>
+                </div>
             </div>
+
+            <div className="project-card">
+                <div className="img">
+                    <a href="https://github.com/jhuang0420/portfolio/tree/master" target="_blank" rel="noopener noreferrer">
+                        <img src={project1Image} alt="Project 1" className="project-image" />
+                    </a>
+                </div>
+                <div className="txt">
+                    <div className="project-title">Project 1</div>
+                    <div className="project-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vel lectus sit amet metus egestas facilisis. Mauris mattis ante in metus facilisis, ut porttitor neque pretium. Praesent hendrerit massa lacus, non elementum nisl vulputate ac. Praesent nibh velit, posuere mattis leo eu, vestibulum interdum metus. Morbi pellentesque ultricies blandit. Nullam rutrum consequat iaculis.</div>
+                </div>
+            </div>
+
         </section>
     );
 }
