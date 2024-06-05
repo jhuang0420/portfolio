205ae015db05248afb56e4e31101165d36df4bfe
Jeffrey Huang <jhuang0420@gmail.com>
2024-06-05 14:14:37 -0400

    updated projects section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,8 @@
 // src/components/Projects.js
 import React from 'react';
 import '../style/Projects.css';
-import project1Image from '../assets/p1.jpg';
+import projectImage from '../assets/p1.jpg';
+import project1Image from '../assets/webdev.png';
 
 function Projects() {
     return (
@@ -14,19 +15,21 @@ function Projects() {
                     </a>
                 </div>
                 <div className="txt">
-                    <div className="project-title">Project 1</div>
-                    <div className="project-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vel lectus sit amet metus egestas facilisis. Mauris mattis ante in metus facilisis, ut porttitor neque pretium. Praesent hendrerit massa lacus, non elementum nisl vulputate ac. Praesent nibh velit, posuere mattis leo eu, vestibulum interdum metus. Morbi pellentesque ultricies blandit. Nullam rutrum consequat iaculis.</div>
+                    <div className="project-title">React Portfolio Site</div>
+                    <div className="project-description">I developed my current website from scratch using React's component-based architecture, which allows for modular and reusable components. By using CSS, I created a responsive and visually appealing design. The site is hosted on GitHub Pages, providing seamless deployment and easy updates.</div>
+                    <div className="project-technologies">React | HTML/CSS | JavaScript | Git</div>
                 </div>
             </div>
 
             <div className="project-card">
                 <div className="txt">
-                    <div className="project-title">Project 1</div>
+                    <div className="project-title">Project 2</div>
                     <div className="project-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vel lectus sit amet metus egestas facilisis. Mauris mattis ante in metus facilisis, ut porttitor neque pretium. Praesent hendrerit massa lacus, non elementum nisl vulputate ac. Praesent nibh velit, posuere mattis leo eu, vestibulum interdum metus. Morbi pellentesque ultricies blandit. Nullam rutrum consequat iaculis.</div>
+                    <div className="project-technologies">React | HTML/CSS | JavaScript | Git</div>
                 </div>
                 <div className="img">
                     <a href="https://github.com/jhuang0420/portfolio/tree/master" target="_blank" rel="noopener noreferrer">
-                        <img src={project1Image} alt="Project 1" className="project-image" />
+                        <img src={projectImage} alt="Project 2" className="project-image" />
                     </a>
                 </div>
             </div>
@@ -34,11 +37,11 @@ function Projects() {
             <div className="project-card">
                 <div className="img">
                     <a href="https://github.com/jhuang0420/portfolio/tree/master" target="_blank" rel="noopener noreferrer">
-                        <img src={project1Image} alt="Project 1" className="project-image" />
+                        <img src={projectImage} alt="Project 3" className="project-image" />
                     </a>
                 </div>
                 <div className="txt">
-                    <div className="project-title">Project 1</div>
+                    <div className="project-title">Project 3</div>
                     <div className="project-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vel lectus sit amet metus egestas facilisis. Mauris mattis ante in metus facilisis, ut porttitor neque pretium. Praesent hendrerit massa lacus, non elementum nisl vulputate ac. Praesent nibh velit, posuere mattis leo eu, vestibulum interdum metus. Morbi pellentesque ultricies blandit. Nullam rutrum consequat iaculis.</div>
                 </div>
             </div>
