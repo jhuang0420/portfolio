3e8502195c3c92cef83036ee5ae837b7637eb801
Jeffrey Huang <jhuang0420@gmail.com>
2024-06-11 15:40:30 -0400

    removed skills and contact

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import About from './components/About';
 import Projects from './components/Projects';
-import Skills from './components/Skills';
-import Contact from './components/Contact';
 import './style/App.css';
 
 
@@ -15,8 +13,6 @@ function App() {
             <Navbar />
             <About />
             <Projects />
-            <Skills />
-            <Contact />
             <Footer />
         </div>
     );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,22 @@
 // src/components/Navbar.js
 import React from 'react';
-import '../style/Navbar.css';
-import logo from '../assets/logo.png'; 
+import '../style/Navbar.css'; 
 
 function Navbar() {
   return (
     <nav className="navbar">
       <div className="navbar-left">
-        <img src={logo} alt="Logo" className="logo" />
+        <h>Jeffrey Huang</h>
       </div>
       <div className="navbar-right">
         <a href="#about">About Me</a>
         <a href="#projects">Projects</a>
-        <a href="#skills">Skills</a>
-        <a href="#contact">Contact</a>
+        <a href="https://docs.google.com/document/d/1H8tIQHWLyUikJExpHshH4Jk69ACFzAMb6HB3FeKi6mo/edit?usp=sharing"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="resume-link">
+                    <i className="fas fa-file"></i> Resume
+                </a>
       </div>
     </nav>
   );
